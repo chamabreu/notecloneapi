a9aa9b08d247135cfe7b78efcb03441f05560d3e
chamabreu <janmanuelbrenner@gmail.com>
2020-11-11 17:06:57 +0100

    no cookiesetting

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   cookie: {
     maxAge: 1000 * 60 * 30,
-    secure: true,
-    sameSite: false
+    // secure: true,
+    // sameSite: false
   },
   store: new MongoStore({ mongooseConnection: mongoose.connection })
 }))
